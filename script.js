ada86c96723392b54eaf8e69fe7649d5225d504e
benlevi220 <111444313+benlevi220@users.noreply.github.com>
2023-01-04 20:00:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let ben_r = ["usa","east coast","west coast","visa",]  
+console.log("ben_r",ben_r , typeof ben_r);
+
+window.onload = function(){
+let id_input =document.querySelector("#id_input");
+let innerHTML = document.querySelector("id_input").innerHTML;
+id_input.innerHTML = "name";
+innerHTML = "approve"
+console.log("id_input",id_input, typeof "id_input");
+}
+
